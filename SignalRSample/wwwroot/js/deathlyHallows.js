543ce3d16a6c3d1a907f6c5d01dc0f8cfe806b2c
2640 <tommylin@ltedu.com.tw>
2025-02-07 11:59:43 +0800

    加入專案檔案。

---
+++ SignalRSample/wwwroot/js/deathlyHallows.js
@@ -0,0 +1,34 @@
+﻿var cloakSpan = document.getElementById("cloakCounter");
+var stoneSpan = document.getElementById("stoneCounter");
+var wandSpan = document.getElementById("wandCounter");
+
+//create connection
+var connectionDeathlyHallows = new signalR.HubConnectionBuilder()
+    //.configureLogging(signalR.LogLevel.Information)
+    .withUrl("/hubs/deathyhallows").build();
+
+//connect to methods that hub invokes aka receive notfications from hub
+connectionDeathlyHallows.on("updateDealthyHallowCount", (cloak, stone, wand) => {
+    cloakSpan.innetText = cloak.toString();
+    stoneSpan.innetText = stone.toString();
+    wandSpan.innetText = wand.toString();
+});
+
+//invoke hub methods aka send notification to hub
+
+//start connection
+function fulfilled() {
+    connectionDeathlyHallows.invoke("GetRaceStatus").then((raceCounter) => {
+        cloakSpan.innerText = raceCounter.cloak.toString();
+        stoneSpan.innerText = raceCounter.stone.toString();
+        wandSpan.innerText = raceCounter.wand.toString();
+    });
+    // do something on start
+    console.log("connection to Dealthy Hallow Hub Successful");
+}
+
+function rejected() {
+    //rejected logs
+}
+
+connectionDeathlyHallows.start().then(fulfilled, rejected);
---
+++ SignalRSample/wwwroot/js/usersCount.js
@@ -0,0 +1,33 @@
+﻿//create connection
+var connectionUserCount = new signalR.HubConnectionBuilder()
+    //.configureLogging(signalR.LogLevel.Information)
+    .withUrl("/hubs/userCount", signalR.HttpTransportType.WebSockets).build();
+
+//connect to methods that hub invokes aka receive notfications from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.invoke("NewWindowLoaded", "Bhrugen").then((value) => console.log(value));
+}
+
+//start connection
+function fulfilled() {
+    // do something on start
+    console.log("connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    //rejected logs
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
